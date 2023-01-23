b3884d2002a9fd128b40ae8632ada25cd40a08f9
Deeps <kmdeepika98@gmail.com>
2023-01-23 09:24:32 +0100

    fixed bug dim

--- pcdet/models/roi_heads/roi_head_template.py
+++ pcdet/models/roi_heads/roi_head_template.py
@@ -97,7 +97,7 @@ class RoIHeadTemplate(nn.Module):
             cls_preds = batch_cls_preds[batch_mask]
 
             cur_roi_scores, cur_roi_labels = torch.max(cls_preds, dim=1)
-            cur_frs_scores = torch.sum(cls_preds)
+            cur_frs_scores = torch.sum(cls_preds,dim=1)
             # alpha = 0.6
             # cur_frs_scores = ((1-alpha)*cur_roi_scores) + (alpha*torch.sigmoid(cur_frs_scores))
             if nms_config.MULTI_CLASSES_NMS:
