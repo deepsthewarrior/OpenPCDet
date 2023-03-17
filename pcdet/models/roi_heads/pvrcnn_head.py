9cd1b9eaec028f2c2555901b3ae43dffd24e4860
Deeps <kmdeepika98@gmail.com>
2023-03-17 11:32:54 +0100

    only_sh

--- pcdet/models/roi_heads/pvrcnn_head.py
+++ pcdet/models/roi_heads/pvrcnn_head.py
@@ -33,18 +33,26 @@ class PVRCNNHead(RoIHeadTemplate):
 
         self.shared_fc_layer = nn.Sequential(*shared_fc_list)
 
-        self.cls_layers_1 = self.make_half_fc_layers(
+        # self.cls_layers_1 = self.make_half_fc_layers(
+        #     input_channels=pre_channel, output_channels=self.num_class, fc_list=self.model_cfg.CLS_FC
+        # )
+        # self.cls_layers_2 = self.make_final_fc_layers(
+        #     input_channels=pre_channel, output_channels=self.num_class, fc_list=self.model_cfg.CLS_FC
+        # )
+        # self.reg_layers_1 = self.make_half_fc_layers(
+        #     input_channels=pre_channel,
+        #     output_channels=self.box_coder.code_size * self.num_class,
+        #     fc_list=self.model_cfg.REG_FC
+        # )
+        # self.reg_layers_2 = self.make_final_fc_layers(
+        #     input_channels=pre_channel,
+        #     output_channels=self.box_coder.code_size * self.num_class,
+        #     fc_list=self.model_cfg.REG_FC
+        # )
+        self.cls_layers = self.make_fc_layers(
             input_channels=pre_channel, output_channels=self.num_class, fc_list=self.model_cfg.CLS_FC
         )
-        self.cls_layers_2 = self.make_final_fc_layers(
-            input_channels=pre_channel, output_channels=self.num_class, fc_list=self.model_cfg.CLS_FC
-        )
-        self.reg_layers_1 = self.make_half_fc_layers(
-            input_channels=pre_channel,
-            output_channels=self.box_coder.code_size * self.num_class,
-            fc_list=self.model_cfg.REG_FC
-        )
-        self.reg_layers_2 = self.make_final_fc_layers(
+        self.reg_layers = self.make_fc_layers(
             input_channels=pre_channel,
             output_channels=self.box_coder.code_size * self.num_class,
             fc_list=self.model_cfg.REG_FC
@@ -72,7 +80,7 @@ class PVRCNNHead(RoIHeadTemplate):
                     init_func(m.weight)
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
-        nn.init.normal_(self.reg_layers_2[-1].weight, mean=0, std=0.001)
+        nn.init.normal_(self.reg_layers[-1].weight, mean=0, std=0.001)
 
     def roi_grid_pool(self, batch_dict):
         """
@@ -172,11 +180,13 @@ class PVRCNNHead(RoIHeadTemplate):
 
         # x = pooled_features.view(batch_size_rcnn, -1, 1).clone().detach()
         shared_features = self.shared_fc_layer(pooled_features.view(batch_size_rcnn, -1, 1))
-        # rcnn_cls = self.cls_layers(shared_features).transpose(1, 2).contiguous().squeeze(dim=1)  # (B, 1 or 2)
-        rcnn_cls_interim = self.cls_layers_1(shared_features)
-        rcnn_cls = self.cls_layers_2(rcnn_cls_interim).transpose(1, 2).contiguous().squeeze(dim=1)
-        rcnn_reg_interim = self.reg_layers_1(shared_features)
-        rcnn_reg = self.reg_layers_2(rcnn_reg_interim).transpose(1, 2).contiguous().squeeze(dim=1)
+        rcnn_cls = self.cls_layers(shared_features).transpose(1, 2).contiguous().squeeze(dim=1)  # (B, 1 or 2)
+        rcnn_reg = self.reg_layers(shared_features).transpose(1, 2).contiguous().squeeze(dim=1)  # (B, C)
+
+        # rcnn_cls_interim = self.cls_layers_1(shared_features)
+        # rcnn_cls = self.cls_layers_2(rcnn_cls_interim).transpose(1, 2).contiguous().squeeze(dim=1)
+        # rcnn_reg_interim = self.reg_layers_1(shared_features)
+        # rcnn_reg = self.reg_layers_2(rcnn_reg_interim).transpose(1, 2).contiguous().squeeze(dim=1)
 
         if not self.training or self.predict_boxes_when_training:
             batch_cls_preds, batch_box_preds = self.generate_predicted_boxes(
@@ -187,8 +197,8 @@ class PVRCNNHead(RoIHeadTemplate):
             batch_dict['batch_box_preds'] = batch_box_preds
             batch_dict['cls_preds_normalized'] = False
             batch_dict['shared_features'] = shared_features.view(batch_box_preds.shape[0],-1,shared_features.shape[1])
-            batch_dict['rcnn_cls_interim'] = rcnn_cls_interim.view(batch_box_preds.shape[0],-1,rcnn_cls_interim.shape[1])
-            batch_dict['rcnn_reg_interim'] = rcnn_reg_interim.view(batch_box_preds.shape[0],-1,rcnn_reg_interim.shape[1])
+            # batch_dict['rcnn_cls_interim'] = rcnn_cls_interim.view(batch_box_preds.shape[0],-1,rcnn_cls_interim.shape[1])
+            # batch_dict['rcnn_reg_interim'] = rcnn_reg_interim.view(batch_box_preds.shape[0],-1,rcnn_reg_interim.shape[1])
             
             # Temporarily add infos to targets_dict for metrics
             targets_dict['batch_box_preds'] = batch_box_preds
