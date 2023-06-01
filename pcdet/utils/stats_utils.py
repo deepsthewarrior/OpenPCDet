e49c6d674f78377690d661441564c3d6ba7aae1b
Deeps <kmdeepika98@gmail.com>
2023-06-01 18:59:54 +0200

    added plots

--- pcdet/utils/stats_utils.py
+++ pcdet/utils/stats_utils.py
@@ -326,7 +326,7 @@ class KITTIEvalMetrics(Metric):
     full_state_update: bool = False
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.reset_state_interval = kwargs.get('reset_state_interval', 64)
+        self.reset_state_interval = kwargs.get('reset_state_interval', 8)
         self.tag = kwargs.get('tag', None)
         self.dataset = kwargs.get('dataset', None)
         current_classes = self.dataset.class_names
